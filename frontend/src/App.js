7e45c2783a8e1a0539da5e5eb573ddc911b18a8e
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-11-04 00:56:27 +0100

    fetch working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react'
 function App() {
+  const [data, setData] = useState(null)
+
+  //   Testing fetch from frontend and it works :)
+  useEffect(() => {
+    fetch('http://localhost:8000/api/stories')
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result)
+        console.log('data', result)
+      })
+  }, [])
   return (
     <div className='App'>
-      <h1>Storytime</h1>
+      <ol>
+        {data?.map((story) => (
+          <li key={story._id}>{story.text}</li>
+        ))}
+      </ol>
     </div>
   )
 }
