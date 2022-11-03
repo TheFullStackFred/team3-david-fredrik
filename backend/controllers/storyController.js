ed6a0b03eb54ae5b5b06f333e4e3f34db6e02a8d
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-11-03 21:27:52 +0100

    REST api for stories

--- backend/controllers/storyController.js
+++ backend/controllers/storyController.js
@@ -1,9 +1,12 @@
 const asyncHandler = require('express-async-handler')
 
+const Story = require('../models/storyModel')
+
 // Get stories
 // GET /api/stories
 const getStories = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get stories' })
+  const stories = await Story.find()
+  res.status(200).json(stories)
 })
 
 // Add story
@@ -13,21 +16,45 @@ const addStory = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Please add a text field')
   }
-  res.status(200).json({ message: 'Add story' })
+
+  const story = await Story.create({
+    text: req.body.text
+  })
+
+  res.status(200).json(story)
 })
 
 // Update story
 // PUT /api/stories/:id
 const updateStory = asyncHandler(async (req, res) => {
+  const story = await Story.findById(req.params.id)
+
+  if (!story) {
+    res.status(400)
+    throw new Error('Story not found')
+  }
+
+  const updatedStory = await Story.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  })
   {
-    res.status(200).json({ message: `Update story ${req.params.id}` })
+    res.status(200).json(updatedStory)
   }
 })
 
 // Delete goal
 // Delete /api/stories/:id
 const deleteStory = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete story ${req.params.id}` })
+  const story = await Story.findById(req.params.id)
+
+  if (!story) {
+    res.status(400)
+    throw new Error('Story not found')
+  }
+
+  await story.remove()
+
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = { getStories, addStory, updateStory, deleteStory }
