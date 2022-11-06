fa7759fa353cb8587f0e98c29782ae023c9f9589
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-11-06 13:41:33 +0100

    router and fetch modified

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+
+const Home = () => {
+  const [stories, setStories] = useState(null)
+
+  useEffect(() => {
+    const fetchStories = async () => {
+      const response = await fetch('http://locaslhosst:8000/api/stories')
+      const json = await response.json()
+      console.log(json)
+
+      if (response.ok) {
+        setStories(json)
+      }
+    }
+
+    fetchStories()
+  }, [])
+
+  return (
+    <div className='home'>
+      <div className='stories'>
+        {stories
+          ? stories?.map((story) => (
+              <div className='story' key={story._id}>
+                <h2>{story.stage1.first}</h2>
+              </div>
+            ))
+          : 'Something went wrong'}
+         
+      </div>
+    </div>
+  )
+}
+
+export default Home
