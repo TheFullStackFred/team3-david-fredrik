7417a3cb798582f7403571c968f3a6dcfabf6ee3
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-11-06 17:52:47 +0100

    fixed image from cdn

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { AdvancedImage } from '@cloudinary/react'
+import { Cloudinary } from '@cloudinary/url-gen'
+
+import { fill } from '@cloudinary/url-gen/actions/resize'
 
 const Welcome = () => {
-  return <div className='welcome'></div>
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'dn8hfzpm2'
+    }
+  })
+
+  const myImage = cld.image('book_juxky0')
+
+  return (
+    <div className='welcome'>
+      <div>
+        <AdvancedImage className='image' cldImg={myImage} />
+      </div>
+    </div>
+  )
 }
 
 export default Welcome
