75e390581db3d234882e19f4fab1c2a8a0398f08
DMould123 <95115610+DMould123@users.noreply.github.com>
2022-11-15 13:13:50 +0100

    Sidebar v2

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+  }
   return (
-    <div className='contact'>
-      <h1>Contact Us</h1>
+    <div id='contact'>
+      <form onSubmit={sendEmail }>
+        <h1>Contact Us</h1>
+        <label>Name</label>
+        <input type='text' name='name' />
+        <label>Email</label>
+        <input type='email' name='email' />
+        <label>Message</label>
+        <textarea name='message' />
+        <input id='submit' type='submit' value='Send' />
+      </form>
     </div>
   )
 }
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -4,8 +4,9 @@ const NotFound = () => {
   return (
     <div className="nf">
     <h1>Oops! You seem to be lost here.</h1>
+    <img src="https://www.whats-on-netflix.com/wp-content/uploads/2017/12/banner1-Lost-620x351.jpg" alt="David" />
     <p>Let's take you back home</p>
-    <Link to='/'>Home</Link>
+    <p><button class="home-button"><Link to='/'>Home</Link></button></p>
 </div>
   )
 }
