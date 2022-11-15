89a8a32f96c81ddf8714780122972ff7255e58eb
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-11-15 14:43:48 +0100

    ..

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,44 +1,22 @@
-import React from 'react'
-
 const AboutUs = () => {
   return (
     <>
-<<<<<<< HEAD
-      <div class="about-section">
+      <div className="about-section">
         <h1>About Us</h1>
         <p>Some text about who we and what we do should go here.</p>
-=======
-<div className="about-section">
-  <h1>About Us</h1>
-  <p>Some text about who we and what we do should go here.</p>
-</div>
-
-<h2>Our Team</h2>
-  <div className="about-column">
-    <div className="about-card">
-      <img src="https://avatars.githubusercontent.com/u/90246884?v=4" alt="Fred"
-      />
-      <div className="about-container">
-        <h2>Fredrik Strömberg</h2>
-        <p className="about-title">FullStack Developer</p>
-        <p className="lorem-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae amet at commodi nesciunt perspiciatis. Molestias, ab esse iste dolor voluptates minima. Sapiente tempore nemo officiis dignissimos cumque repellat nam rem.</p>
-        <p>fred@storytime.se</p>
-        <p><button className="about-button">Contact Me</button></p>
->>>>>>> b2ed6df403da79334aa17b296433ea5f5f3239c6
       </div>
 
-<<<<<<< HEAD
       <h2>Our Team</h2>
-      <div class="about-column">
-        <div class="about-card">
+      <div className="about-column">
+        <div className="about-card">
           <img
             src="https://avatars.githubusercontent.com/u/90246884?v=4"
             alt="Fred"
           />
-          <div class="about-container">
+          <div className="about-container">
             <h2>Fredrik Strömberg</h2>
-            <p class="about-title">FullStack Developer</p>
-            <p class="lorem-text">
+            <p className="about-title">FullStack Developer</p>
+            <p className="lorem-text">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Repudiandae amet at commodi nesciunt perspiciatis. Molestias, ab
               esse iste dolor voluptates minima. Sapiente tempore nemo officiis
@@ -46,24 +24,22 @@ const AboutUs = () => {
             </p>
             <p>fred@storytime.se</p>
             <p>
-              <button class="about-button">
-                <a href="mailto:fredrik.stromberg@iths.se">Contact Me</a>
-              </button>
+              <button className="about-button">Contact Me</button>
             </p>
           </div>
         </div>
       </div>
 
-      <div class="about-column">
-        <div class="about-card">
+      <div className="about-column">
+        <div className="about-card">
           <img
             src="https://avatars.githubusercontent.com/u/95115610?v=4"
             alt="David"
           />
-          <div class="about-container">
+          <div className="about-container">
             <h2>David Mould</h2>
-            <p class="about-title">JavaScript Developer</p>
-            <p class="lorem-text">
+            <p className="about-title">JavaScript Developer</p>
+            <p className="lorem-text">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
               ipsum maiores omnis nostrum praesentium atque esse enim veniam
               veritatis iste? Harum tempora, minus magni atque minima cum
@@ -71,23 +47,10 @@ const AboutUs = () => {
             </p>
             <p>david@storytime.com</p>
             <p>
-              <button class="about-button">
-                <a href="mailto:david.mould@iths.se">Contact Me</a>
-              </button>
+              <button className="about-button">Contact Me</button>
             </p>
           </div>
         </div>
-=======
-  <div className="about-column">
-    <div className="about-card">
-      <img src="https://avatars.githubusercontent.com/u/95115610?v=4" alt="David" />
-      <div className="about-container">
-        <h2>David Mould</h2>
-        <p className="about-title">JavaScript Developer</p>
-        <p className="lorem-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsum maiores omnis nostrum praesentium atque esse enim veniam veritatis iste? Harum tempora, minus magni atque minima cum voluptate possimus perspiciatis!</p>
-        <p>david@storytime.com</p>
-        <p><button className="about-button">Contact Me</button></p>
->>>>>>> b2ed6df403da79334aa17b296433ea5f5f3239c6
       </div>
     </>
   )
