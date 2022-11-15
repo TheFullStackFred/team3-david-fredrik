d5197a008dfe9f5108ee126adc2f17359db1b211
DMould123 <95115610+DMould123@users.noreply.github.com>
2022-11-15 14:02:38 +0100

    updated classname

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -3,35 +3,35 @@ import React from 'react'
 const AboutUs = () => {
   return (
     <>
-<div class="about-section">
+<div className="about-section">
   <h1>About Us</h1>
   <p>Some text about who we and what we do should go here.</p>
 </div>
 
 <h2>Our Team</h2>
-  <div class="about-column">
-    <div class="about-card">
+  <div className="about-column">
+    <div className="about-card">
       <img src="https://avatars.githubusercontent.com/u/90246884?v=4" alt="Fred"
       />
-      <div class="about-container">
+      <div className="about-container">
         <h2>Fredrik Strömberg</h2>
-        <p class="about-title">FullStack Developer</p>
-        <p class="lorem-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae amet at commodi nesciunt perspiciatis. Molestias, ab esse iste dolor voluptates minima. Sapiente tempore nemo officiis dignissimos cumque repellat nam rem.</p>
+        <p className="about-title">FullStack Developer</p>
+        <p className="lorem-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae amet at commodi nesciunt perspiciatis. Molestias, ab esse iste dolor voluptates minima. Sapiente tempore nemo officiis dignissimos cumque repellat nam rem.</p>
         <p>fred@storytime.se</p>
-        <p><button class="about-button">Contact Me</button></p>
+        <p><button className="about-button">Contact Me</button></p>
       </div>
     </div>
   </div>
 
-  <div class="about-column">
-    <div class="about-card">
+  <div className="about-column">
+    <div className="about-card">
       <img src="https://avatars.githubusercontent.com/u/95115610?v=4" alt="David" />
-      <div class="about-container">
+      <div className="about-container">
         <h2>David Mould</h2>
-        <p class="about-title">JavaScript Developer</p>
-        <p class="lorem-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsum maiores omnis nostrum praesentium atque esse enim veniam veritatis iste? Harum tempora, minus magni atque minima cum voluptate possimus perspiciatis!</p>
+        <p className="about-title">JavaScript Developer</p>
+        <p className="lorem-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsum maiores omnis nostrum praesentium atque esse enim veniam veritatis iste? Harum tempora, minus magni atque minima cum voluptate possimus perspiciatis!</p>
         <p>david@storytime.com</p>
-        <p><button class="about-button">Contact Me</button></p>
+        <p><button className="about-button">Contact Me</button></p>
       </div>
     </div>
   </div>
